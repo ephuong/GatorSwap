db4fa1bc7fbe1a5bf9e5e3f7590f9c39d79dec10
Daniel Martin <danielmartin@Daniels-MacBook-Pro.local>
2016-07-19 01:46:24 -0700

    Changeing the panels

--- application/view/home/index.php
+++ application/view/home/index.php
@@ -18,23 +18,23 @@
   <div class="row">
     <div class="col-sm-4">
       <div class="panel panel-primary">
-        <div class="panel-heading">BOOKS</div>
+        <div class="panel-heading">Books</div>
         <!--USING flaticon free icons here, need to put in doc -->
-        <div class="panel-body"><img src="http://image.flaticon.com/icons/svg/126/126503.svg"  alt="Image"  width="200" height="200"></div>
+        <div class="panel-body"><img src="http://image.flaticon.com/icons/svg/126/126503.svg"  alt="Image"  width="300" height="200"></div>
         <div class="panel-footer">Click here to buy books</div>
       </div>
     </div>
     <div class="col-sm-4">
       <div class="panel panel-primary">
-        <div class="panel-heading">OFFICE SUPPLIES</div>
-        <div class="panel-body"><img src="C:\sayali\CSC648\office_supplies.png"  alt="Image" width="300" height="300"></div>
+        <div class="panel-heading">Office</div>
+        <div class="panel-body"><img src="http://image.flaticon.com/icons/svg/126/126483.svg"  alt="Image" width="300" height="200"></div>
         <div class="panel-footer">Click here to buy office supplies</div>
       </div>
     </div>
     <div class="col-sm-4">
       <div class="panel panel-primary">
-        <div class="panel-heading">CLOTHES</div>
-        <div class="panel-body"><img src="http://saintnicholastemecula.org/wp-content/uploads/2015/06/garment-clothing-tags.jpg" width="300" height="300"alt="Image"></div>
+        <div class="panel-heading">Clothing</div>
+        <div class="panel-body"><img src="http://image.flaticon.com/icons/svg/126/126515.svg" width="300" height="200"alt="Image"></div>
         <div class="panel-footer">Click here to buy clothes</div>
       </div>
     </div>
@@ -45,22 +45,22 @@
   <div class="row">
     <div class="col-sm-4">
       <div class="panel panel-primary">
-        <div class="panel-heading">FURNITURE</div>
-        <div class="panel-body"><img src="http://doc.mo.gov/mve/images/school/NewDorm.jpg"  height= "300" width ="300" alt="Image"></div>
+        <div class="panel-heading">Furniture</div>
+        <div class="panel-body"><img src="http://image.flaticon.com/icons/svg/126/126496.svg"  height= "200" width ="300" alt="Image"></div>
         <div class="panel-footer">Click here to buy furniture</div>
       </div>
     </div>
     <div class="col-sm-4">
       <div class="panel panel-primary">
-        <div class="panel-heading">ELECTRONIC SUPPLIES</div>
-        <div class="panel-body"><img src="http://images.wisegeek.com/rifiuti-elettronici.jpg" height= "300" width ="300" alt="Image"></div>
+        <div class="panel-heading">Electronics</div>
+        <div class="panel-body"><img src="http://image.flaticon.com/icons/svg/126/126512.svg" height= "200" width ="300" alt="Image"></div>
         <div class="panel-footer">Click here to buy electronic supplies</div>
       </div>
     </div>
     <div class="col-sm-4">
       <div class="panel panel-primary">
-        <div class="panel-heading">DEALS(subject to change)</div>
-        <div class="panel-body"><img src="http://www.6aming.com/wp-content/uploads/2014/03/video-game-deals.jpg"  height= "300" width ="300" alt="Image"></div>
+        <div class="panel-heading">Other</div>
+        <div class="panel-body"><img src="http://image.flaticon.com/icons/svg/126/126498.svg"  height= "200" width ="300" alt="Image"></div>
         <div class="panel-footer">Click here to browse deals</div>
       </div>
     </div>
