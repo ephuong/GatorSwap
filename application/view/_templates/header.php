aaf35ecf7784fbdffdd908a2785456948ea93bef
Daniel Martin <danielmartin@Daniels-MacBook-Pro.local>
2016-07-19 01:11:30 -0700

    Cleaning up the header and the footer

--- application/view/_templates/header.php
+++ application/view/_templates/header.php
@@ -75,9 +75,9 @@
 	  </div>
     </div>
       <ul class="nav navbar-nav navbar-right">
-        <li><a href="#"><span class="glyphicon glyphicon-shopping-cart headerLinks"></span> Cart</a></li>
-        <li><a href="<?php echo URL; ?>home/signin"><span class="glyphicon glyphicon-log-in headerLinks"></span>Sign In</a></li>
-		<li><a href="<?php echo URL; ?>home/profile"><span class="glyphicon glyphicon-user headerLinks"></span>Profile</a></li>
+        <li><a href="#" class="headerLinks"><span class="glyphicon glyphicon-shopping-cart headerLinks"></span> Cart</a></li>
+        <li><a href="<?php echo URL; ?>home/signin" class="headerLinks"><span class="glyphicon glyphicon-log-in headerLinks"></span>Sign In</a></li>
+		<li><a href="<?php echo URL; ?>home/profile" class="headerLinks"><span class="glyphicon glyphicon-user headerLinks"></span>Profile</a></li>
       </ul>
     </div>
   </div>
