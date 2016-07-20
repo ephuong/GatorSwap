c1ee32e1cbb3334753abf1be01f04319ed34ea5d
Daniel Martin <danielmartin@sfs-wifi-dhcp-10-143-84-201.wifi.sfsu.edu>
2016-07-20 14:30:03 -0700

    Added Footer pages and adding content

--- application/view/home/privacy.php
+++ application/view/home/privacy.php
@@ -16,53 +16,6 @@
 
 <div class="container">
   <div class="row">
-    <div class="col-sm-4">
-      <div class="panel panel-primary">
-        <div class="panel-heading">Books</div>
-        <!--USING flaticon free icons here, need to put in doc -->
-        <div class="panel-body"><img src="http://image.flaticon.com/icons/svg/126/126503.svg"  alt="Image"  width="300" height="200"></div>
-        <div class="panel-footer">Click here to buy books</div>
-      </div>
-    </div>
-    <div class="col-sm-4">
-      <div class="panel panel-primary">
-        <div class="panel-heading">Office</div>
-        <div class="panel-body"><img src="http://image.flaticon.com/icons/svg/126/126483.svg"  alt="Image" width="300" height="200"></div>
-        <div class="panel-footer">Click here to buy office supplies</div>
-      </div>
-    </div>
-    <div class="col-sm-4">
-      <div class="panel panel-primary">
-        <div class="panel-heading">Clothing</div>
-        <div class="panel-body"><img src="http://image.flaticon.com/icons/svg/126/126515.svg" width="300" height="200"alt="Image"></div>
-        <div class="panel-footer">Click here to buy clothes</div>
-      </div>
-    </div>
-  </div>
-</div><br>
-
-<div class="container">
-  <div class="row">
-    <div class="col-sm-4">
-      <div class="panel panel-primary">
-        <div class="panel-heading">Furniture</div>
-        <div class="panel-body"><img src="http://image.flaticon.com/icons/svg/126/126496.svg"  height= "200" width ="300" alt="Image"></div>
-        <div class="panel-footer">Click here to buy furniture</div>
-      </div>
-    </div>
-    <div class="col-sm-4">
-      <div class="panel panel-primary">
-        <div class="panel-heading">Electronics</div>
-        <div class="panel-body"><img src="http://image.flaticon.com/icons/svg/126/126512.svg" height= "200" width ="300" alt="Image"></div>
-        <div class="panel-footer">Click here to buy electronic supplies</div>
-      </div>
-    </div>
-    <div class="col-sm-4">
-      <div class="panel panel-primary">
-        <div class="panel-heading">Other</div>
-        <div class="panel-body"><img src="http://image.flaticon.com/icons/svg/126/126498.svg"  height= "200" width ="300" alt="Image"></div>
-        <div class="panel-footer">Click here to browse deals</div>
-      </div>
-    </div>
+    Privacy
   </div>
 </div><br><br>
