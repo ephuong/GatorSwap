0f9a490d5b2c0d052c8ba023a24738d3b3d4dbf2
Daniel Martin <danielmartin@sfs-wifi-dhcp-10-143-84-201.wifi.sfsu.edu>
2016-07-20 14:41:47 -0700

    Added Footer pages and adding content

--- application/view/_templates/footer.php
+++ application/view/_templates/footer.php
@@ -1,13 +1,14 @@
 <footer class="container-fluid text-center">
 	<div class="container">
 		<div class="row">
-			<div class="col-md-2 col-md-offset-1"><a href="" title="About us" class="footerLinks"> About</a> </div>
-    		<div class="col-md-2"><a href="" title="Help" class="footerLinks"> Help</a></div>
-    		<div class="col-md-2"><a href="" title="Privacy policy" class="footerLinks"> Privacy</a></div>
-    		<div class="col-md-2"><a href="" title="Contact_info" class="footerLinks"> Contact info</a></div>
-        	<div class="col-md-2"><a href="" title="Terms and conditions" class="footerLinks"> Terms and conditions</a></div>
+			<div class="col-md-2 col-md-offset-1"><a href="<?php echo URL; ?>home/about" title="About us" class="footerLinks"> About</a> </div>
+    		<div class="col-md-2"><a href="<?php echo URL; ?>home/about" title="Help" class="footerLinks"> Help</a></div>
+    		<div class="col-md-2"><a href="<?php echo URL; ?>home/about" title="Privacy policy" class="footerLinks"> Privacy</a></div>
+    		<div class="col-md-2"><a href="<?php echo URL; ?>home/about" title="Contact_info" class="footerLinks"> Contact info</a></div>
+        	<div class="col-md-2"><a href="<?php echo URL; ?>home/about" title="Terms and conditions" class="footerLinks"> Terms and conditions</a></div>
 		</div> <!--Row-->
   	</div> <!--container-->
 </footer>
 </body>
 </html>
+
