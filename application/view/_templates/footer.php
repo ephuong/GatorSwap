3b2521fe36dc202ce82af321315b69d594d27c40
Daniel Martin <danielmartin@sfs-wifi-dhcp-10-143-84-201.wifi.sfsu.edu>
2016-07-20 14:48:30 -0700

    Formatting header and correct redirects

--- application/view/_templates/footer.php
+++ application/view/_templates/footer.php
@@ -1,11 +1,23 @@
-<footer class="container-fluid text-center">
+<style>
+
+#gatorSwapFooter{
+	width:100%;
+	position: absolute;
+	height: 80px;
+	clear:both;
+	top:100%;
+}
+</style>
+
+
+<footer class="container-fluid text-center" id="gatorSwapFooter">
 	<div class="container">
 		<div class="row">
 			<div class="col-md-2 col-md-offset-1"><a href="<?php echo URL; ?>home/about" title="About us" class="footerLinks"> About</a> </div>
-    		<div class="col-md-2"><a href="<?php echo URL; ?>home/about" title="Help" class="footerLinks"> Help</a></div>
-    		<div class="col-md-2"><a href="<?php echo URL; ?>home/about" title="Privacy policy" class="footerLinks"> Privacy</a></div>
-    		<div class="col-md-2"><a href="<?php echo URL; ?>home/about" title="Contact_info" class="footerLinks"> Contact info</a></div>
-        	<div class="col-md-2"><a href="<?php echo URL; ?>home/about" title="Terms and conditions" class="footerLinks"> Terms and conditions</a></div>
+    		<div class="col-md-2"><a href="<?php echo URL; ?>home/help" title="Help" class="footerLinks"> Help</a></div>
+    		<div class="col-md-2"><a href="<?php echo URL; ?>home/privacy" title="Privacy policy" class="footerLinks"> Privacy</a></div>
+    		<div class="col-md-2"><a href="<?php echo URL; ?>home/contact" title="Contact_info" class="footerLinks"> Contact info</a></div>
+        	<div class="col-md-2"><a href="<?php echo URL; ?>home/terms" title="Terms and conditions" class="footerLinks"> Terms and conditions</a></div>
 		</div> <!--Row-->
   	</div> <!--container-->
 </footer>
