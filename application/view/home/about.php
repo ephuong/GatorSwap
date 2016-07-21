46b3b92e091c50c1aaa459db18602ae9a8d3069f
Daniel Martin <danielmartin@Daniels-MacBook-Pro.local>
2016-07-21 01:30:27 -0700

    Fixing up about page

--- application/view/home/about.php
+++ application/view/home/about.php
@@ -15,7 +15,23 @@
 
 
 <div class="container">
-  <div class="page-header">
-    <h1>ABOUT</h1>
-  </div>
-</div><br><br>
+	<div class="row">
+  		<div class="page-header">
+    		<h1>About GatorSwap</h1>
+    		<small>SFSU's Marketplace</small>
+  		</div>
+
+  		<p>
+  			GatorSwap is San Francisco State University's online marketplace, connecting students campus-wide to buy, sell, and trade their belongings! 
+  		</p>
+  	</div>
+
+  	<div class="row">
+  		<h3>Mission Statement</h3>
+  		<p>
+  			GatorSwap's mission is to connect San Francisco State University students so that they can buy, sell, and trade with ease. 
+  		</p>
+  	</div>
+</div>
+
+<br><br>
