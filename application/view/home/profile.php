39e4d69e8df9d42f4caf7e1a54f39fa18e7f4b81
Sayali Modkharkar <sayali@sfsuswe.com>
2016-07-21 04:57:28 +0000

    changed profile page

--- application/view/home/profile.php
+++ application/view/home/profile.php
@@ -1,80 +1,248 @@
 
-  <style>
-    /* Remove the jumbotron's default bottom margin */
-     .jumbotron {
-      margin-bottom: 0;
-    }
-   .box-content {
-	display: inline-block;
-	width: 200px;
-    padding: 10px;
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>CSC 648 - Group 7</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+  <style><style>
+ @import "bourbon";
+
+body {
+	background: #eee !important;	
 }
 
-.bottom {
-	border-bottom: 1px solid #ccc;
+.wrapper {	
+	margin-top: 80px;
+  margin-bottom: 80px;
 }
 
-.right {
-	border-right: 1px solid #ccc;
+.form-signin {
+  max-width: 380px;
+  padding: 15px 35px 45px;
+  margin: 0 auto;
+  background-color: #fff;
+  border: 1px solid rgba(0,0,0,0.1);  
+
+  .form-signin-heading,
+	.checkbox {
+	  margin-bottom: 30px;
+	}
+
+	.checkbox {
+	  font-weight: normal;
+	}
+
+	.form-control {
+	  position: relative;
+	  font-size: 16px;
+	  height: auto;
+	  padding: 10px;
+		@include box-sizing(border-box);
+
+		&:focus {
+		  z-index: 2;
+		}
+	}
+
+	input[type="text"] {
+	  margin-bottom: -1px;
+	  border-bottom-left-radius: 0;
+	  border-bottom-right-radius: 0;
+	}
+
+	input[type="password"] {
+	  margin-bottom: 20px;
+	  border-top-left-radius: 0;
+	  border-top-right-radius: 0;
+	}
+       .Title {
+        text-align: center;
+	line-height: 300%;
 }
+}
+
+.form-signup {
+  max-width: 80%;
+  padding: 15px 35px 45px;
+  margin: 0 auto;
+  background-color: #fff;
+  border: 1px solid rgba(0,0,0,0.1);  
+
+  .form-signup-heading,
+	.checkbox {
+	  margin-bottom: 30px;
+	}
+
+	.checkbox {
+	  font-weight: normal;
+	}
+
+	.form-control {
+	  position: relative;
+	  font-size: 16px;
+	  height: auto;
+	  padding: 10px;
+		@include box-sizing(border-box);
+
+		&:focus {
+		  z-index: 2;
+		}
+	}
+
+	input[type="text"] {
+	  margin-bottom: -1px;
+	  border-bottom-left-radius: 0;
+	  border-bottom-right-radius: 0;
+	}
+
+	input[type="password"] {
+	  margin-bottom: 20px;
+	  border-top-left-radius: 0;
+	  border-top-right-radius: 0;
+	}
+       .Title {
+        text-align: center;
+	line-height: 300%;
+}
+}
+.button{
+	
+}
+</style>
+
+ 
+
+<div id="signup" class="container">
     
-  </style>
-</head>
-<body>
-<div class="container">
-  <div class="row">
-    <div class="col-sm-4">
-      <div class="panel panel-primary">
-        <div class="panel-heading">BOOKS</div>
-        <div class="panel-body"><img src="http://www.gijobs.com/wp-content/uploads/2014/08/textbooks.resized-1.jpg"  alt="Image" width="300" height="300" ></div>
-        <div class="panel-footer">Click here to buy books</div>
-      </div>
-    </div>
-    <div class="col-sm-4">
-      <div class="panel panel-primary">
-        <div class="panel-heading">OFFICE SUPPLIES</div>
-        <div class="panel-body"><img src="C:\sayali\CSC648\office_supplies.png"  alt="Image" width="300" height="300"></div>
-        <div class="panel-footer">Click here to buy office supplies</div>
-      </div>
-    </div>
-    <div class="col-sm-4">
-      <div class="panel panel-primary">
-        <div class="panel-heading">CLOTHES</div>
-        <div class="panel-body"><img src="http://saintnicholastemecula.org/wp-content/uploads/2015/06/garment-clothing-tags.jpg" width="300" height="300"alt="Image"></div>
-        <div class="panel-footer">Click here to buy clothes</div>
-      </div>
-    </div>
-  </div>
-</div><br>
+    <form class="form-signup" role="form" method="post" action="#">
+        <h3>Account Information</h3>
+        <div class="row">
+            <div class="col-md-6">
+                
+                <div class="form-group">
+	                <label>First Name</label>
+	                <input class="form-control" name="firstname" placeholder="Your First Name" type="text">
+            	</div>
 
-<div class="container">
-  <div class="row">
-    <div class="col-sm-4">
-      <div class="panel panel-primary">
-        <div class="panel-heading">FURNITURE</div>
-        <div class="panel-body"><img src="http://doc.mo.gov/mve/images/school/NewDorm.jpg"  height= "300" width ="300" alt="Image"></div>
-        <div class="panel-footer">Click here to buy furniture</div>
-      </div>
-    </div>
-    <div class="col-sm-4">
-      <div class="panel panel-primary">
-        <div class="panel-heading">ELECTRONIC SUPPLIES</div>
-        <div class="panel-body"><img src="http://images.wisegeek.com/rifiuti-elettronici.jpg" height= "300" width ="300" alt="Image"></div>
-        <div class="panel-footer">Click here to buy electronic supplies</div>
-      </div>
-    </div>
-    <div class="col-sm-4">
-      <div class="panel panel-primary">
-        <div class="panel-heading">DEALS(subject to change)</div>
-        <div class="panel-body"><img src="http://www.6aming.com/wp-content/uploads/2014/03/video-game-deals.jpg"  height= "300" width ="300" alt="Image"></div>
-        <div class="panel-footer">Click here to browse deals</div>
-      </div>
-    </div>
-  </div>
-</div><br><br>
+            	<div class="form-group">
+	                <label>Last Name</label>
+	                <input class="form-control" name="lastname" placeholder="Your Last Name" type="text">
+            	</div>
+
+		  <div class="form-group">                
+	                <label>UserName</label> 
+	                <input class="form-control" name="username" placeholder="Username" type="text"> 
+            	</div>
+                
+                <div class="form-group">                
+	                <label>Password</label> 
+	                <input class="form-control" name="password" placeholder="Password" type="text"> 
+            	</div>
+                
+                <div class="form-group">                
+	                <label>Confirm Password</label> 
+	                <input class="form-control" name="confirmPassword" placeholder="Confirm Password" type="text"> 
+            	</div>
+                
+               
+                
+
+            </div>
+            
+            <div class="col-md-6">
+                
+                <div class="form-group">                
+	                <label>SFSU E-mail</label> 
+	                <input class="form-control" name="email" placeholder="....@mail.sfsu.edu" type="email" data-error="Bruh, that email address is invalid" required> 
+            	        
+                </div>
+                
+                <div class="form-group">                
+	                <label>SFSU ID:</label> 
+	                <input class="form-control" name="sfsu_id" placeholder="SFSU ID" type="text"> 
+            	</div>
+                
+                <div class="form-group">                
+	                <label>Phone Number</label> 
+	                <input class="form-control" name="phoneNumber" placeholder="Phone Number" type="text"> 
+            	</div>
+
+   		
+                
+                <div class="form-group">                
+	                <label>Address:</label> 
+	                <input class="form-control" name="address"  placeholder="1600 Holloway Ave" type="text"> 
+            	</div>
+        <div class="form-group">                
+	                <label>City</label> 
+	                <input class="form-control"  name="city" placeholder="City" type="text"> 
+            	</div>
+        <div class="form-group">                
+	                <label>Zipcode</label> 
+	                <input class="form-control" name="zipcode" placeholder="Zipcode" type="text"> 
+            	</div>
+				</div>
+        <div class = "container-text-centre">
+         <button class="btn btn-lg btn-primary btn-block" type="submit">Save</button>   
+</div>		 
+        
+                
+       </div>
+                  
+    </form>
+</div>
+</br></br></br></br></br>
+<script>
+  
+   $('#backToTopBtn').click(function(){
+        $('html,body').animate({scrollTop:0},'slow');return false;
+    }); 
+   // $(".Title").on("click", function( e ) {
+        
+       // e.preventDefault();
+    
+       // $("body, html").animate({ 
+       //     scrollTop: $( $(this).attr('href') ).offset().top 
+      //  }, 5000);
+        
+    //});
+    
+    $('a[href^="#"]').on('click', function(event) {
+    var target = $(this.getAttribute('href'));
+    if( target.length ) {
+        event.preventDefault();
+        $('html, body').stop().animate({
+            scrollTop: target.offset().top
+        }, 10000);
+    }
+});
+    </script>    
+
+
+<?php
+if(isset($_POST['submit'])){
+    $firstname = $_POST['firstname'];
+ $lastname = $_POST['lastname'];
+ $username = $_POST['username'];
+ 
+ $password = $_POST['password'];
+ $confirmPassword = $_POST['confirmPassword'];
+ $email = $_POST['email'];
+ $sfsu_id = $_POST['sfsu_id'];
+ $phoneNumber = $_POST['phoneNumber'];
+ $address = $_POST['address'];
+ $city = $_POST['city'];
+ $zipcode = $_POST['zipcode'];
+}
+ 
+ 
+?>
 
 
 </body>
-
-
+</html>
 
