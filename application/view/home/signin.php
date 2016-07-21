679dcebb4db01ffba1a4a924b4be0651dd0d4d07
Dipro Chowdhury <achowdh1@mail.sfsu.edu>
2016-07-21 02:01:01 +0000

    SignIn/RegistrationForm Done

--- application/view/home/signin.php
+++ application/view/home/signin.php
@@ -1,78 +1,228 @@
-<div class="container">
-  <h2>Sign in / Registration</h2>
-  <p>Type your username and password to sign in.</p>
-  <form role="form">
-    <div class="form-inline">
-      <label for="usr">UserName:
-      <input type="text" class="form-control" id="usr" placeholder="Username" required>
-	  </label>
-    </div>
-    <div class="form-inline">
-      <label for="pwd">Password: <input type="password" class="form-control" id="pwd" placeholder="Password" required>
-	  </label>
-    </div>
-  </form>
+<style>
+ @import "bourbon";
 
-</div>
-<div class="container">
-  <input type="Done" class="btn btn-info" value="Done">
-</div><br><br>
+body {
+	background: #eee !important;	
+}
+
+.wrapper {	
+	margin-top: 80px;
+  margin-bottom: 80px;
+}
+
+.form-signin {
+  max-width: 380px;
+  padding: 15px 35px 45px;
+  margin: 0 auto;
+  background-color: #fff;
+  border: 1px solid rgba(0,0,0,0.1);  
+
+  .form-signin-heading,
+	.checkbox {
+	  margin-bottom: 30px;
+	}
+
+	.checkbox {
+	  font-weight: normal;
+	}
+
+	.form-control {
+	  position: relative;
+	  font-size: 16px;
+	  height: auto;
+	  padding: 10px;
+		@include box-sizing(border-box);
+
+		&:focus {
+		  z-index: 2;
+		}
+	}
+
+	input[type="text"] {
+	  margin-bottom: -1px;
+	  border-bottom-left-radius: 0;
+	  border-bottom-right-radius: 0;
+	}
+
+	input[type="password"] {
+	  margin-bottom: 20px;
+	  border-top-left-radius: 0;
+	  border-top-right-radius: 0;
+	}
+       .Title {
+        text-align: center;
+	line-height: 300%;
+}
+}
+
+.form-signup {
+  max-width: 80%;
+  padding: 15px 35px 45px;
+  margin: 0 auto;
+  background-color: #fff;
+  border: 1px solid rgba(0,0,0,0.1);  
+
+  .form-signup-heading,
+	.checkbox {
+	  margin-bottom: 30px;
+	}
+
+	.checkbox {
+	  font-weight: normal;
+	}
+
+	.form-control {
+	  position: relative;
+	  font-size: 16px;
+	  height: auto;
+	  padding: 10px;
+		@include box-sizing(border-box);
+
+		&:focus {
+		  z-index: 2;
+		}
+	}
+
+	input[type="text"] {
+	  margin-bottom: -1px;
+	  border-bottom-left-radius: 0;
+	  border-bottom-right-radius: 0;
+	}
+
+	input[type="password"] {
+	  margin-bottom: 20px;
+	  border-top-left-radius: 0;
+	  border-top-right-radius: 0;
+	}
+       .Title {
+        text-align: center;
+	line-height: 300%;
+}
+}
+</style>
+<script>
+  
+   $('#backToTopBtn').click(function(){
+        $('html,body').animate({scrollTop:0},'slow');return false;
+    }); 
+   // $(".Title").on("click", function( e ) {
+        
+       // e.preventDefault();
+    
+       // $("body, html").animate({ 
+       //     scrollTop: $( $(this).attr('href') ).offset().top 
+      //  }, 5000);
+        
+    //});
+    
+    $('a[href^="#"]').on('click', function(event) {
+    var target = $(this.getAttribute('href'));
+    if( target.length ) {
+        event.preventDefault();
+        $('html, body').stop().animate({
+            scrollTop: target.offset().top
+        }, 10000);
+    }
+});
+        
+
+</script>
+
+  <div id="login" class="wrapper">
+    <form class="form-signin">       
+      <h3>Login or <a href="#signup">Sign up</a> </h3>
+      <input type="text" class="form-control" name="username" placeholder="Username" required="" autofocus="" /></br>
+      <input type="password" class="form-control" name="password" placeholder="Password" required=""/>      
+      <label class="checkbox">
+        <input type="checkbox" value="remember-me" id="rememberMe" name="rememberMe"> Remember me
+      </label>
+      <button class="btn btn-lg btn-primary btn-block" type="submit">Login</button>   
+    </form>
+  </div>
+
 <hr>
-<div class="container">
-  <h2> Register To Create Your Account</h2>
-  <form class="form-horizontal role="form">
-    <div class="form-inline">
-      <label for="firstname">Name  First:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-      <input type="text" class="form-control" id="firstname" placeholder="Firstname" required>
-	  </label>
+</br></br></br></br>
 
-      <label for="lastname"> Last: <input type="text" class="form-control" id="lastname" placeholder="Lastname" required>
-	  </label>
-    </div>
-	<div class="form-inline">
-      <label for="usr"> Username: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="text" class="form-control" id="usr" placeholder="Type your Username" required>
-	  </label>
-    </div>
-	<div class="form-inline">
-      <label for="usr"> Password: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="text" class="form-control" id="pass" placeholder="Type Your Password" required>
-	  </label><br>
-	  <label for="usr">Confirm password: <input type="text" class="form-control" id="usr" placeholder="Your password again" required>
-	  </label>
-    </div>
-<hr>
-	<div class="form-inline">
-      <label for="ads"> Address: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="text" class="form-control" id="ads" placeholder="1600, Holloway Ave" required>
-	  </label>
-    </div>
+<div id="signup" class="container">
+    
+    <form class="form-signup" role="form">
+        <h3>Register To Create Your Account</h3>
+        <div class="row">
+            <div class="col-md-4">
+                
+                <div class="form-group">
+	                <label>First Name</label>
+	                <input class="form-control" placeholder="Your First Name" type="text">
+            	</div>
 
-	<div class="form-inline">
-      <label for="city"> City: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="text" class="form-control" id="city" placeholder="Example:San Francisco" required>
-	  </label>
-    </div>
+            	<div class="form-group">
+	                <label>Last Name</label>
+	                <input class="form-control" placeholder="Your Last Name" type="text">
+            	</div>
 
-	<div class="form-inline">
-      <label for="number"> Zip:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;<input type="text" class="form-control" id="zip" placeholder="Example:94132" required>
-	  </label>
-    </div>
-	<hr>
-	<br><br>
-	 <div class="form-inline">
-      <label for="email">SFSU email:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-      <input type="text" class="form-control" id="email" placeholder="john@example.com" required>
-	  </label>
-	  </div>
-	<div class="form-inline">
-      <label for="id"> SFSU ID:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;<input type="text" class="form-control" id="id" placeholder="Your 9 digit sfsu ID" required>
-	  </label>
-    </div>
+		  <div class="form-group">                
+	                <label>UserName</label> 
+	                <input class="form-control" placeholder="Username" type="text"> 
+            	</div>
+                
+                <div class="form-group">                
+	                <label>Password</label> 
+	                <input class="form-control" placeholder="Password" type="text"> 
+            	</div>
+                
+                <div class="form-group">                
+	                <label>Confirm Password</label> 
+	                <input class="form-control" placeholder="Confirm Password" type="text"> 
+            	</div>
+                
+                <h3>Already Have an account?</br><a href="#top" id ="backToTopBtn">Login</a></h3>
+                
 
-	<div class="form-inline">
-      <label for="num"> Phone no:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="text" class="form-control" id="num" placeholder="408-123-456" required>
-	  </label>
-    </div>
-  </form>
+            </div>
+            
+            <div class="col-md-4">
+                
+                <div class="form-group">                
+	                <label>SFSU E-mail</label> 
+	                <input class="form-control" placeholder="....@mail.sfsu.edu" type="text"> 
+            	</div>
+                
+                <div class="form-group">                
+	                <label>SFSU ID:</label> 
+	                <input class="form-control" placeholder="SFSU ID" type="text"> 
+            	</div>
+                
+                <div class="form-group">                
+	                <label>Phone Number</label> 
+	                <input class="form-control" placeholder="Phone Number" type="text"> 
+            	</div>
 
+   		
+                
+                <div class="form-group">                
+	                <label>Address:</label> 
+	                <input class="form-control" placeholder="1600 Holloway Ave" type="text"> 
+            	</div>
+        <div class="form-group">                
+	                <label>City</label> 
+	                <input class="form-control" placeholder="City" type="text"> 
+            	</div>
+        <div class="form-group">                
+	                <label>Zipcode</label> 
+	                <input class="form-control" placeholder="Zipcode" type="text"> 
+            	</div>
+        
+         <button class="btn btn-lg btn-primary btn-block" type="submit">REGISTER</button>                 
+        </div>
+                
+       </div>
+                  
+    </form>
 </div>
-<div class="container">
-  <input type="Done" class="btn btn-info" value="Done">
-</div><br><br>
+</br></br></br></br></br>
+
+
+
+
+
+
