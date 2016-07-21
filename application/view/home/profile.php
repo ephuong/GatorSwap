e82493d916b06a18e9b28968ecae73f52e346990
dpmartin <dpmartin@mail.sfsu.edu>
2016-07-21 04:43:46 +0000

    changed profil page

--- application/view/home/profile.php
+++ application/view/home/profile.php
@@ -1,54 +1,80 @@
+
+  <style>
+    /* Remove the jumbotron's default bottom margin */
+     .jumbotron {
+      margin-bottom: 0;
+    }
+   .box-content {
+	display: inline-block;
+	width: 200px;
+    padding: 10px;
+}
+
+.bottom {
+	border-bottom: 1px solid #ccc;
+}
+
+.right {
+	border-right: 1px solid #ccc;
+}
+    
+  </style>
+</head>
+<body>
 <div class="container">
-  <h2> Profile Page</h2>
-  <p>Hi! Here is the information you gave us, you can choose to edit this page if you want.</p>
-  <form class="form-horizontal role="form">
-    <div class="form-inline">
-      <label for="firstname">Name  First:
-      <input type="text" class="form-control" id="firstname">
-	  </label>
+  <div class="row">
+    <div class="col-sm-4">
+      <div class="panel panel-primary">
+        <div class="panel-heading">BOOKS</div>
+        <div class="panel-body"><img src="http://www.gijobs.com/wp-content/uploads/2014/08/textbooks.resized-1.jpg"  alt="Image" width="300" height="300" ></div>
+        <div class="panel-footer">Click here to buy books</div>
+      </div>
+    </div>
+    <div class="col-sm-4">
+      <div class="panel panel-primary">
+        <div class="panel-heading">OFFICE SUPPLIES</div>
+        <div class="panel-body"><img src="C:\sayali\CSC648\office_supplies.png"  alt="Image" width="300" height="300"></div>
+        <div class="panel-footer">Click here to buy office supplies</div>
+      </div>
+    </div>
+    <div class="col-sm-4">
+      <div class="panel panel-primary">
+        <div class="panel-heading">CLOTHES</div>
+        <div class="panel-body"><img src="http://saintnicholastemecula.org/wp-content/uploads/2015/06/garment-clothing-tags.jpg" width="300" height="300"alt="Image"></div>
+        <div class="panel-footer">Click here to buy clothes</div>
+      </div>
+    </div>
+  </div>
+</div><br>
 
-      <label for="lastname"> Last: <input type="text" class="form-control" id="lastname">
-	  </label>
-    </div>
-
-	<div class="form-inline">
-      <label for="ads"> Address: &nbsp;&nbsp;&nbsp;&nbsp;<input type="text" class="form-control" id="ads">
-	  </label>
-    </div>
-
-	<div class="form-inline">
-      <label for="city"> City: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="text" class="form-control" id="city">
-	  </label>
-    </div>
-
-	<div class="form-inline">
-      <label for="number"> Zip:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;<input type="text" class="form-control" id="zip">
-	  </label>
-    </div>
-	<p>__________________________________________________________________________________________________________________________________________________________</p>
-	<br><br>
-	 <div class="form-inline">
-      <label for="email">SFSU email:
-      <input type="text" class="form-control" id="email">
-	  </label>
-	  </div>
-	<div class="form-inline">
-      <label for="id"> SFSU ID:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <input type="text" class="form-control" id="id">
-	  </label>
-    </div>
-	<div class="form-inline">
-      <label for="usr"> Username: &nbsp;&nbsp;<input type="text" class="form-control" id="usr">
-	  </label>
-    </div>
-	<div class="form-inline">
-      <label for="num"> Phone no:&nbsp;&nbsp; <input type="text" class="form-control" id="num">
-	  </label>
-    </div>
-  </form>
-
-</div>
 <div class="container">
-
-  <button type="button" class="btn btn-info btn-md">  Save   </button>
+  <div class="row">
+    <div class="col-sm-4">
+      <div class="panel panel-primary">
+        <div class="panel-heading">FURNITURE</div>
+        <div class="panel-body"><img src="http://doc.mo.gov/mve/images/school/NewDorm.jpg"  height= "300" width ="300" alt="Image"></div>
+        <div class="panel-footer">Click here to buy furniture</div>
+      </div>
+    </div>
+    <div class="col-sm-4">
+      <div class="panel panel-primary">
+        <div class="panel-heading">ELECTRONIC SUPPLIES</div>
+        <div class="panel-body"><img src="http://images.wisegeek.com/rifiuti-elettronici.jpg" height= "300" width ="300" alt="Image"></div>
+        <div class="panel-footer">Click here to buy electronic supplies</div>
+      </div>
+    </div>
+    <div class="col-sm-4">
+      <div class="panel panel-primary">
+        <div class="panel-heading">DEALS(subject to change)</div>
+        <div class="panel-body"><img src="http://www.6aming.com/wp-content/uploads/2014/03/video-game-deals.jpg"  height= "300" width ="300" alt="Image"></div>
+        <div class="panel-footer">Click here to browse deals</div>
+      </div>
+    </div>
+  </div>
 </div><br><br>
+
+
+</body>
+
+
 
