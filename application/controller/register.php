a1bc6f54b407ec271fc1dd42875608d2ddd9ac0e
Dipro Chowdhury <achowdh1@mail.sfsu.edu>
2016-07-22 22:43:29 +0000

    resgiter#changed

--- application/controller/register.php
+++ application/controller/register.php
@@ -21,8 +21,8 @@ class Register extends Controller
           //Insert new row in Account and setting the User's name in database using values inputted in the HTML form
           $newly_registered_account_id = $this->AccountModel->registerAccount($_POST["username"],$_POST["password"],$_POST["sfsu_id"]);
 			
-	  $this->UserModel->setUser($newly_registered_account_id,$_POST["firstname"], $_POST["lastname"],$_POST["country"],$_POST["state"],$_POST["address"],$_POST["city"],$_POST["zipcode"],$_POST["phoneNumber"]);
-			
+	 // $this->UserModel->setUser($newly_registered_account_id,$_POST["firstname"], $_POST["lastname"],$_POST["country"],$_POST["state"],$_POST["address"],$_POST["city"],$_POST["zipcode"],$_POST["phoneNumber"]);
+	  callSetUser();		
         } 
         else 
         {
@@ -34,7 +34,10 @@ class Register extends Controller
 	    // where to go after song has been added
             header('location: ' . URL . 'dashboard/index');
 	}    
-        
+    public function callSetUser()
+            {
+        $this->UserModel->setUser($newly_registered_account_id,$_POST["firstname"], $_POST["lastname"],$_POST["country"],$_POST["state"],$_POST["address"],$_POST["city"],$_POST["zipcode"],$_POST["phoneNumber"]);
+    }    
       
 }
 
