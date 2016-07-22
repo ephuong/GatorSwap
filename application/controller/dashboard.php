03ea7f9c9a57357024fd4f5c56b027ca8d5ae4c1
Dipro Chowdhury <achowdh1@mail.sfsu.edu>
2016-07-22 05:47:15 +0000

    Fixed..

--- application/controller/dashboard.php
+++ application/controller/dashboard.php
@@ -2,6 +2,12 @@
 
 class Dashboard extends Controller
 {
-	
+	public function dashboard()
+    {
+        // load views
+        require APP . 'view/_templates/header.php';
+        require APP . 'view/home/dashboard.php';
+        require APP . 'view/_templates/footer.php';
+    }
 				
 }
