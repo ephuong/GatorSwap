72a106af4a9032ae53fb9fa517a7e8aed0f3941e
Dipro Chowdhury <achowdh1@mail.sfsu.edu>
2016-07-22 22:09:53 +0000

    sql#updated.

--- application/model/User.php
+++ application/model/User.php
@@ -17,8 +17,9 @@ class User extends Model
 			
 			
 		       //$query1->execute($parameters1);
-                        
-                       $sql = "INSERT INTO User (Account_ID,F_Name, L_Name,Country,State,Address,City,Zipcode,Phone) VALUES (:account_id,:firstname, :lastname,:country,:state,:address,:city,:zipcode,:phoneNumber)";
+                       //$sql = "UPDATE User SET WHERE Account_ID = :account_id,F_Name= :firstname,L_Name=:lastname,Country=:country,State=:state,Address=:address,City=:city,Zipcode=:zipcode,Phone=:phoneNumber;" 
+                       //$sql = "INSERT INTO User (Account_ID,F_Name, L_Name,Country,State,Address,City,Zipcode,Phone) VALUES (:account_id,:firstname, :lastname,:country,:state,:address,:city,:zipcode,:phoneNumber)";
+                       $sql = "UPDATE User SET F_Name= :firstname,L_Name=:lastname,Country=:country,State=:state,Address=:address,City=:city,Zipcode=:zipcode,Phone=:phoneNumber WHERE Account_ID = :account_id;";
                        $query = $this->db->prepare($sql);
                        $parameters = array(':account_id'=>$account_id,':firstname' => $firstname, ':lastname' => $lastname,':country'=>$country,':state'=>$state,':address'=>$address,':city'=>$city,':zipcode'=>$zipcode,':phoneNumber'=>$phoneNumber);
 		       $query->execute($parameters); 
