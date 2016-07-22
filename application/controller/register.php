81be50ff61a48ab5e09499564b4602dd0ec3ce69
Dipro Chowdhury <achowdh1@mail.sfsu.edu>
2016-07-22 22:56:36 +0000

    addUser#updated.

--- application/controller/register.php
+++ application/controller/register.php
@@ -20,8 +20,10 @@ class Register extends Controller
 			
           //Insert new row in Account and setting the User's name in database using values inputted in the HTML form
           $newly_registered_account_id = $this->AccountModel->registerAccount($_POST["username"],$_POST["password"],$_POST["sfsu_id"]);
-			
-	 $this->UserModel->setUser($newly_registered_account_id,$_POST["firstname"], $_POST["lastname"],$_POST["country"],$_POST["state"],$_POST["address"],$_POST["city"],$_POST["zipcode"],$_POST["phoneNumber"]);
+	  
+          $user = new User;
+	  //$this->UserModel->
+          $user->setUser($newly_registered_account_id,$_POST["firstname"], $_POST["lastname"],$_POST["country"],$_POST["state"],$_POST["address"],$_POST["city"],$_POST["zipcode"],$_POST["phoneNumber"]);
 	  		
         } 
         else 
--- application/model/User.php
+++ application/model/User.php
@@ -7,11 +7,10 @@ class User extends Model
 	/**
 	 * Set's the user's full name in the database when they first register
 	 */
-	public function setUser($account_id,$firstname, $lastname,$country,$state,$address, $city, $zipcode,$phoneNumber) {
-		
+      public function setUser($account_id,$firstname, $lastname,$country,$state,$address, $city, $zipcode,$phoneNumber) 
+        {	
             try {	
-                    
-                       $sql = "UPDATE User SET F_Name= :firstname,L_Name=:lastname,Country=:country,State=:state,Address=:address,City=:city,Zipcode=:zipcode,Phone=:phoneNumber WHERE Account_ID = :account_id";
+                       $sql = "UPDATE User SET F_Name= :firstname,L_Name=:lastname,Country=:country,State=:state,Address=:address,City=:city,Zipcode=:zipcode,Phone=:phoneNumber WHERE Account_ID = :account_id;";
                        $query = $this->db->prepare($sql);
                        $parameters = array(':account_id'=>$account_id,':firstname' => $firstname, ':lastname' => $lastname,':country'=>$country,':state'=>$state,':address'=>$address,':city'=>$city,':zipcode'=>$zipcode,':phoneNumber'=>$phoneNumber);
 		       $query->execute($parameters); 
