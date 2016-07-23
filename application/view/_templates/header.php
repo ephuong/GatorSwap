5c822ad426523b68680b5ce89eae57a1e5b452bf
Daniel Martin <danielmartin@Daniels-MacBook-Pro.local>
2016-07-23 13:36:17 -0700

    Fixing the header

--- application/view/_templates/header.php
+++ application/view/_templates/header.php
@@ -87,7 +87,7 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="#">Logo</a>
+      <a class="navbar-brand" href="<?php echo URL; ?>home/index">Logo</a>
     </div>
     <div class="collapse navbar-collapse" id="myNavbar">
 	   <div class="form-inline">
