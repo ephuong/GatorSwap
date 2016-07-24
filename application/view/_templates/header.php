725ff92526425d56798f2f7b0c5640615649d26b
Dipro Chowdhury <achowdh1@mail.sfsu.edu>
2016-07-24 00:06:14 +0000

    invalid#signInTest

--- application/view/_templates/header.php
+++ application/view/_templates/header.php
@@ -7,7 +7,8 @@
   <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
   <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-  
+  <script src="../sweetalert/dist/sweetalert.min.js"></script>
+  <link rel="stylesheet" type="text/css" href="../sweetalert/dist/sweetalert.css">
   <style>
     /* Remove the navbar's default rounded borders and increase the bottom margin */
     .navbar {
