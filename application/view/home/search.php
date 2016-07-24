98913f838109c7e67750ddb6833d54f072bbbd01
Sayali Modkharkar <sayali@sfsuswe.com>
2016-07-24 01:27:34 +0000

    edited search page

--- application/view/home/search.php
+++ application/view/home/search.php
@@ -1,22 +1,4 @@
- <style>    /* Set height of the grid so .sidenav can be 100% (adjust if needed) */
-    .row.content {height: 1500px}
-    
-    /* Set gray background color and 100% height */
-    .sidenav {
-      background-color: #f1f1f1;
-      height: 100%;
-    }
-    
-    
-    
-    /* On small screens, set height to 'auto' for sidenav and grid */
-    @media screen and (max-width: 767px) {
-      .sidenav {
-        height: auto;
-        padding: 15px;
-      }
-      .row.content {height: auto;}
-    }</style>
+
 
 <div class="container">
 
