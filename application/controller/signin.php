c0caea3b2bc46c80ad15bcf3e994783fb565948d
Dipro Chowdhury <achowdh1@mail.sfsu.edu>
2016-07-24 00:54:50 +0000

    signIN#redirection

--- application/controller/signin.php
+++ application/controller/signin.php
@@ -33,11 +33,11 @@ class Signin extends Controller
           }
             else if(!isset ($_SESSION['login'])|| $_SESSION['login']==false)
         { 
-         header('location: ' . URL . 'sign/index');  
+          
          //User is invalid Warning here
          $message = "Username and/or Password incorrect.\\nTry again.";
           echo "<script type='text/javascript'>alert('$message');</script>";      
-              
+          header('location: ' . URL . 'sign/index');     
         } 
       
       	    
