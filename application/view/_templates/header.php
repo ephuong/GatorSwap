b0d95c792b5733c1a086f1bb401ade1236020d42
Daniel Martin <danielmartin@Daniels-MacBook-Pro.local>
2016-07-25 01:11:00 -0700

    Fixing the header

--- application/view/_templates/header.php
+++ application/view/_templates/header.php
@@ -168,7 +168,7 @@
   </div>
 </nav> -->
 
-<!-- <nav class="navbar navbar-default navbar-inverse" role="navigation">
+<nav class="navbar navbar-default navbar-inverse" role="navigation">
   <div class="navbar-collapse collapse">
     <ul class="nav navbar-nav navbar-left">
         <li><a href="<?php echo URL; ?>home/index">Logo</a></li>
@@ -203,28 +203,6 @@
         <li><a href="<?php echo URL; ?>home/profile" class="headerLinks"><span class="glyphicon glyphicon-user headerLinks"></span> Profile</a></li>
     </ul>
   </div>
-</nav> -->
-
-<nav class="navbar navbar-light bg-faded">
-  <a class="navbar-brand" href="#">Navbar</a>
-  <ul class="nav navbar-nav">
-    <li class="nav-item active">
-      <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="#">Features</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="#">Pricing</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="#">About</a>
-    </li>
-  </ul>
-  <form class="form-inline pull-xs-right">
-    <input class="form-control" type="text" placeholder="Search">
-    <button class="btn btn-success-outline" type="submit">Search</button>
-  </form>
 </nav>
 
  <script>
