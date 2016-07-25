dd8d58d325437a0b5332264002f22db8f8c9a6f0
Daniel Martin <danielmartin@Daniels-MacBook-Pro.local>
2016-07-25 02:34:44 -0700

    Fixing the header

--- application/view/_templates/header.php
+++ application/view/_templates/header.php
@@ -191,11 +191,14 @@
 
 <nav class="navbar navbar-default navbar-inverse" role="navigation">
 
-  <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+  <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
+        <a class="navbar-brand" href="<?php echo URL; ?>home/index">Logo</a>
+    </div>
 
       
   <div class="navbar-collapse collapse">
