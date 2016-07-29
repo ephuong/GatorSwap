75b026f0c7186acc59967343fe51cd6a850ff413
Dipro Chowdhury <achowdh1@mail.sfsu.edu>
2016-07-29 20:04:40 +0000

    Made_sellItemLink_Visible

--- application/view/_templates/header.php
+++ application/view/_templates/header.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+    
 <head>
   <title>CSC 648 - Group 7</title>
   <meta charset="utf-8">
@@ -7,131 +8,12 @@
   <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
   <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-  
-  <style>
-    /* Remove the navbar's default rounded borders and increase the bottom margin */
-    .navbar {
-	   padding: 25px;
-      margin-bottom: 50px;
-      border-radius: 0;
-	   background-color: #26215f;
-
-
-    }
-	.btn {
-    
-    border-color: #46b8da;
-    padding-left: 50px;
-    padding-right: 50px;
-}
-
-    /* Remove the jumbotron's default bottom margin */
-     .jumbotron {
-      margin-bottom: 0;
-    }
-
-    /* Add a gray background color and some padding to the footer */
-    footer {
-      background-color: #53565A;
-      padding: 25px;
-    }
-	.h4, h4{
-		font-size : 20px;
-		color : white;
-		
-		text-decoration: underline;
-		
-	}
-	a {
-                color: #3399ff;
-		text-align: center; 
-	}
-
-    .footerLinks{
-      color:white;
-
-    }
-
-    .headerLinks{
-      color:white;
-
-    }
-    .footerLinks:hover{
-      color:#C99700;
-
-    }
-
-    .headerLinks:hover{
-      color:#C99700;
-
-    }
-
-    .navbar-inverse .navbar-nav>li>a {
-    color: white;
-    }
-
-    .navbar-inverse .navbar-nav>li>a:hover {
-    color: #C99700;
-    }    
-
-  .navbar-brand>img {
-    max-height: 100%;
-    height: 100%;
-    width: auto;
-    margin: 0 auto;
- 
- 
-    /* probably not needed anymore, but doesn't hurt */
-    -o-object-fit: contain;
-    object-fit: contain; 
- 
- }
- 
- .centered-search
- {
-     position: absolute;
-     width: 100%;
-     left: 0;
-     text-align: center;
-     margin:0 auto;
- }
- 
- #search-button
- {
-   padding-left: 15px;
-   padding-right: 15px;
-   margin-top: 10px;
- }
- 
- .search-input {
-   margin-top: 10px;
-   width:450px !important;
-   margin-left: 10px;
-   margin-right: 10px;
- }
- 
- #menuitem
- {
-   padding-left: 20px;
-   padding-right: 20px;
-   margin-right: 5px;
-   margin-top: 10px;
- 
- }
-
- #logo{
-  margin-right: 10px;
-
- }
-
-   
-  </style>
- 
-  
+  <link rel="stylesheet" type="text/css" href="<?php echo URL; ?>public/css/custom.css"> 
 </head>
+
 <body>
 <nav class="navbar navbar-default navbar-inverse" role="navigation">
-  <a class="navbar-left" href="<?php echo URL; ?>home/index"><img id="logo" src="https://s31.postimg.org/5s7zczl7f/Gator_Swap_Logo.jpg" alt="GatorSwap" width="65" height="55"></img></a>
+  <a class="navbar-left" href="<?php echo URL; ?>home/index"><img id="logo" src="https://s31.postimg.org/5s7zczl7f/Gator_Swap_Logo.jpg" alt="GatorSwap" width="65" height="55"></a>
 
   <div class="navbar-header">
       <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
@@ -145,20 +27,7 @@
   <div class="navbar-collapse collapse" id="myNavbar">
     <ul class="nav navbar-nav navbar-left">
         <li>
-            <!-- <div class="dropdown">
-                <button class="btn btn-default dropdown-toggle" type="button" id="menuitem" data-toggle="dropdown">All
-                <span class="caret"></span></button>
-                  <ul class="dropdown-menu" role="menu" aria-labelledby="menuitem">
-                    <li role="presentation"><a role="menuitem" tabindex="1" href="#">All</a></li>
-                    <li role="presentation"><a role="menuitem" tabindex="1" href="#">Books</a></li>
-                    <li role="presentation"><a role="menuitem" tabindex="1" href="#">Office Supplies</a></li>
-                    <li role="presentation"><a role="menuitem" tabindex="1" href="#">Clothes</a></li>
-                    <li role="presentation"><a role="menuitem" tabindex="1" href="#">Furniture</a></li>
-                    <li role="presentation"><a role="menuitem" tabindex="1" href="#">Electronic Supplies</a></li>
-                    <li role="presentation"><a role="menuitem" tabindex="1" href="#">Other</a></li>
-                  </ul>
-              </div> -->
-
+  
               <select class="form-control" id="menuitem">
                 <option value="All">All</option>
                 <option value="Books">Books</option>
@@ -209,7 +78,6 @@
               $('#signinLink').show();
               $('#logoutLink').hide();
               $('#profileLink').hide();
-              $('#sellLink').hide();
               $('#cartLink').hide();
               </script>
              "; 
