b523c327ffa1d6113ee224a3773da5c0e37d2258
Daniel Martin <danielmartin@Daniels-MacBook-Pro.local>
2016-07-29 01:03:35 -0700

    Centered the font and icons in the panels for a better look and feel

--- application/view/home/index.php
+++ application/view/home/index.php
@@ -29,24 +29,23 @@
   <div class="row">
     <div class="col-sm-4">
       <div class="panel panel-primary">
-        <div class="panel-heading">Books</div>
-        <!--USING flaticon free icons here, need to put in doc -->
-        <div class="panel-body"><img src="http://image.flaticon.com/icons/svg/126/126503.svg"  alt="Image"  width="100" height="50"></div>
-        <div class="panel-footer">Click here to view books</div>
+        <div class="panel-heading text-center">Books</div>
+        <div class="panel-body text-center"><img src="http://image.flaticon.com/icons/svg/126/126503.svg"  alt="Image"  width="100" height="50"></div>
+        <div class="panel-footer text-center">Click here to view books</div>
       </div>
     </div>
     <div class="col-sm-4">
       <div class="panel panel-primary">
-        <div class="panel-heading">Office</div>
-        <div class="panel-body"><img src="http://image.flaticon.com/icons/svg/126/126483.svg"  alt="Image" width="100" height="50"></div>
-        <div class="panel-footer">Click here to view office supplies</div>
+        <div class="panel-heading text-center">Office</div>
+        <div class="panel-body text-center"><img src="http://image.flaticon.com/icons/svg/126/126483.svg"  alt="Image" width="100" height="50"></div>
+        <div class="panel-footer text-center">Click here to view office supplies</div>
       </div>
     </div>
     <div class="col-sm-4">
       <div class="panel panel-primary">
-        <div class="panel-heading">Clothing</div>
-        <div class="panel-body"><img src="http://image.flaticon.com/icons/svg/126/126515.svg" width="100" height="50"alt="Image"></div>
-        <div class="panel-footer">Click here to view clothes</div>
+        <div class="panel-heading text-center">Clothing</div>
+        <div class="panel-body text-center"><img src="http://image.flaticon.com/icons/svg/126/126515.svg" width="100" height="50"alt="Image"></div>
+        <div class="panel-footer text-center">Click here to view clothes</div>
       </div>
     </div>
   </div>
@@ -56,23 +55,23 @@
   <div class="row">
     <div class="col-sm-4">
       <div class="panel panel-primary">
-        <div class="panel-heading">Furniture</div>
-        <div class="panel-body"><img src="http://image.flaticon.com/icons/svg/126/126496.svg"  height= "100" width ="50" alt="Image"></div>
-        <div class="panel-footer">Click here to view furniture</div>
+        <div class="panel-heading text-center">Furniture</div>
+        <div class="panel-body text-center"><img src="http://image.flaticon.com/icons/svg/126/126496.svg"  height= "100" width ="50" alt="Image"></div>
+        <div class="panel-footer text-center">Click here to view furniture</div>
       </div>
     </div>
     <div class="col-sm-4">
       <div class="panel panel-primary">
-        <div class="panel-heading">Electronics</div>
-        <div class="panel-body"><img src="http://image.flaticon.com/icons/svg/126/126512.svg" height= "100" width ="50" alt="Image"></div>
-        <div class="panel-footer">Click here to view electronic supplies</div>
+        <div class="panel-heading text-center">Electronics</div>
+        <div class="panel-body text-center"><img src="http://image.flaticon.com/icons/svg/126/126512.svg" height= "100" width ="50" alt="Image"></div>
+        <div class="panel-footer text-center">Click here to view electronic supplies</div>
       </div>
     </div>
     <div class="col-sm-4">
       <div class="panel panel-primary">
-        <div class="panel-heading">Other</div>
-        <div class="panel-body"><img src="http://image.flaticon.com/icons/svg/126/126498.svg"  height= "100" width ="50" alt="Image"></div>
-        <div class="panel-footer">Click here to view everything else</div>
+        <div class="panel-heading text-center">Other</div>
+        <div class="panel-body text-center"><img src="http://image.flaticon.com/icons/svg/126/126498.svg"  height= "100" width ="50" alt="Image"></div>
+        <div class="panel-footer text-center">Click here to view everything else</div>
       </div>
     </div>
   </div>
